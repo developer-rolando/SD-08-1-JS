7c1be2e39dec1fe55a494f1ecfcc1724a2da7374
developer-rolando <rolando.garcia.sanchez1@gmail.com>
2026-01-06 18:44:48 -0600

    Incluyendo metodo levelUp para incrementar en uno el nivel del jugador

--- task4.js
+++ task4.js
@@ -1,7 +1,22 @@
 export class Player {
-    constructor() {
-      
+  name;
+  level;
+    constructor(name, level) {
+      this.name = name;
+      this.level = level;
     }
-  
-    
+
+    info(){
+      return `${this.name} has reached Level ${this.level}!`;
+    }
+
+    levelUp(){
+      this.level += 1;
+    }
   }
+
+  const p1 = new Player("Tara", 6);
+
+  console.log(p1.info());
+  p1.levelUp();
+  console.log(p1.info());
